b5a59556c6c25fc1ed7aa1838a84996d2a38af80
migueldeoleiros <migueldeoleiros@gmail.com>
2022-05-06 11:01:31 +0200

    mallocs y prints arreglados

--- matvec.c
+++ matvec.c
@@ -3,7 +3,7 @@
 #include <mpi.h>
 #include <stdlib.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 #define N 1024
 
@@ -12,10 +12,13 @@ int main(int argc, char *argv[] ) {
     int i, j,k,numprocs,rank;
     int rows;
     int compu_time, transfer_time;
-    float matrix[N][N];
     float vector[N];
-    float result[N];
+    float *matrix;
+    float *result;
     struct timeval tv_compu1, tv_compu2, tv_transfer1, tv_transfer2;
+
+    float *localMatrix;
+    float *localResult;
 
     MPI_Status status;
     MPI_Init(&argc,&argv);
@@ -25,13 +28,15 @@ int main(int argc, char *argv[] ) {
     int sizes[numprocs], desp[numprocs];
 
     // Initialize Matrix and Vector
-        if(rank==0){
-            for(i=0;i<N;i++) {
-                vector[i] = i;
-                for(j=0;j<N;j++)
-                    matrix[i][j] = i+j;
-            }
+    if(rank==0){
+        matrix = malloc(sizeof(float)*N*N);
+        result = malloc(sizeof(float)*N);
+        for(i=0;i<N;i++) {
+            vector[i] = i;
+            for(j=0;j<N;j++)
+                matrix[i+j*N] = i+j;
         }
+    }
 
     gettimeofday(&tv_transfer1, NULL);
     // distribución del vector 
@@ -47,8 +52,8 @@ int main(int argc, char *argv[] ) {
     if(rank == numprocs-1)
         rows = rows+(N%numprocs);
 
-    float *localMatrix = malloc(sizeof(float)*N*rows);
-    float *localResult = malloc(sizeof(float)*N);
+    localMatrix = malloc(sizeof(float)*N*rows);
+    localResult = malloc(sizeof(float)*N);
 
     // creamos sentcounts y displ para statterv
     if(rank==0){
@@ -105,19 +110,29 @@ int main(int argc, char *argv[] ) {
     compu_time = (tv_compu2.tv_usec - tv_compu1.tv_usec)+ 1000000
                  * (tv_compu2.tv_sec - tv_compu1.tv_sec);
 
-    printf ("Computational Time (seconds) of process %d = %lf\n",
-            rank, (double) compu_time/1E6);
-    printf ("Transfer Time (seconds) of process %d = %lf\n",
-            rank, (double) transfer_time/1E6);
     
-    /*Display result */
-    MPI_Barrier(MPI_COMM_WORLD);
-    if(rank==0 && DEBUG){
-        for(i=0;i<N;i++) {
-            printf(" %f \t ",result[i]);
+    if(DEBUG){
+        /*Display result */
+        if(rank==0){
+            for(i=0;i<N;i++) {
+                printf(" %f \t ",result[i]);
+            }
+            printf("\n");
         }
-        printf("\n");
-    }    
+    }else{
+        /*Display times */
+        printf ("process %d:\t Computational Time = %lf\t Transfer Time = %lf\n",
+                rank, (double) compu_time/1E6, (double) transfer_time/1E6);
+        }    
+
+    // free memory
+    if(rank==0){
+        free(matrix);
+        free(result);
+    }
+    free(localMatrix);
+    free(localResult);
+
 
     MPI_Finalize();
     return 0;
